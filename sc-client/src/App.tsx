fc90ec972b4d97b749353357c695e06d4202f434
ferrna <arriondovfernando@gmail.com>
2023-08-15 17:41:53 -0300

    Configuracion inicial de proyecto

--- sc-client/src/App.tsx
+++ sc-client/src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="Body">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <p style={{ fontWeight: 'bold' }}>COSMÉTICA</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
